0009f2855d1cd74c11387152a3b11df06aeb5eff
oscar <oscar.vasquez@zentro.com.mx>
2024-01-16 15:43:25 -0600

    debug

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const {conexion} = require("./basedatos/conexion");
+const express = require("express")
+const cors = require("cors")
 
 //Iniciar app
 console.log("App de node arrancada");
 
 //Conectar a la base de datos
 conexion();
+
+// Crear servidor Node
+const app = express();
+const puerto = 3900;
+
+// Configurar cors
+app.use(cors());
+
+// Convertir body a objeto js
+app.use(express.json())
+
+// Crear rutas
+
+//Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en el puerto "+puerto)
+})
