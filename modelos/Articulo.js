96511fd5a44bceac779936f846c5955b38e8530d
oscar <oscar.vasquez@zentro.com.mx>
2024-01-16 16:45:55 -0600

    debug

---
+++ modelos/Articulo.js
@@ -0,0 +1,23 @@
+const {Schema, model} = require("mongoose");
+
+const ArticuloSchema = Scehma({
+    titulo:{
+        type: String,
+        require: true
+    },
+    contenido:{
+        type: String,
+        require: true
+    },
+    fecha:{
+        type: Date,
+        require: Date.now
+    },
+    imagen: {
+        type: String,
+        default: "deafult.png"
+    }
+});
+
+
+module.export = model("Articulo",ArticuloSchema,"articulos")
